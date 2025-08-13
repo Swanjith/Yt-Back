8259f78fd3001b624c394cb0e66e38ab708437ee
Swanjith <swanjithak19@gmail.com>
2025-08-13 22:35:26 +0530

    login done

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,11 @@
 const express  = require('express');
 const Router = express.Router();
-const  becrypt = require('bcrypt');
+const  bcrypt = require('bcrypt');
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 const User = require('../models/User'); // Assuming you have a User model defined
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 
 
 
@@ -30,7 +30,7 @@ Router.post('/signup', async (req, res) => {
          const uploadImage = await cloudinary.uploader.upload(req.files.logo.tempFilePath)
 
        const newUser = new User({
-        _id:new mongoose.Types.ObjectId,
+        _id:new mongoose.Types.ObjectId(),
         channelName:req.body.channelName,
         email:req.body.email,
         phone:req.body.phone,
@@ -50,6 +50,60 @@ Router.post('/signup', async (req, res) => {
         error:err
     })
     }
+});
+
+Router.post('/login',async(req,res)=>{
+try{
+    // console.log(req.body);
+    const users = await User.find({email:req.body.email})
+    // console.log(users);
+    if(users.length == 0){
+        return res.status(500).json({
+            error:"email not registered"
+        })
+        }
+    const isValid = await bcrypt.compare(req.body.password,users[0].password)
+    // console.log(isValid)
+
+    if(!isValid){
+        return res.status(200).json({
+            error:"invalid password"
+    })
+}
+     const token = jwt.sign({
+        _id:users[0]._id,
+        channelName:users[0].channelName,
+        email:users[0].email,
+        phone:users[0].phone,
+        logoId:users[0].logoId
+     },
+     "swanjith is good",
+     {
+     expiresIn:"365d"
+     }
+)
+    res.status(200).json({
+        _id:users[0]._id,   
+        channelName:users[0].channelName,
+        email:users[0].email,
+        phone:users[0].phone,
+        logoId:users[0].logoId,
+        logoUrl :users[0].logoUrl,
+        token:token,
+        subscribers:users[0].subscribers,
+        subscribedChannels:users[0].subscribedChannels
+    })
+    
+    
+    
+    
+    
+    }catch(err){
+    console.log(err);
+    res.status(500).json({
+        error:"something is wrong"
+    })
+}
 });
 
 module.exports = Router;
